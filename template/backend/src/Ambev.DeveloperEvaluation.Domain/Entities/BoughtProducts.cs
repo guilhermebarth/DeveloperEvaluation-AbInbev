2539897a379a1fc6c2bb34154a19fb8322f44c53
Guilherme <guilhermebarth2018@gmail.com>
2025-02-07 00:47:46 -0300

    feat: Implementação dos DTOs que estarão contidos no Banco de Dados e EF Core. O Automapper será adicionado no próximo commit juntamente com o Mediator.

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/BoughtProducts.cs
@@ -0,0 +1,10 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class BoughtProducts : BaseEntity
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/DeveloperSalesContext.cs
@@ -0,0 +1,48 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Ambev.DeveloperEvaluation.ORM
+{
+    public class DeveloperSalesContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<BoughtProducts> BoughtProducts { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Rating> Rate { get; set; }
+
+        public DeveloperSalesContext(DbContextOptions<DefaultContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+    public class DeveloperSalesDbContextFactory : IDesignTimeDbContextFactory<DefaultContext>
+    {
+        public DefaultContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DefaultContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseNpgsql(
+                   connectionString,
+                   b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.WebApi")
+            );
+
+            return new DefaultContext(builder.Options);
+        }
+    }
+}
