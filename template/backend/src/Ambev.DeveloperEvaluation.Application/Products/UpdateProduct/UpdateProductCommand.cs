702d99a2a768fadca86eb81282f95ed480803f7c
Guilherme <guilhermebarth2018@gmail.com>
2025-02-11 09:47:10 -0300

    fix: Added some missing properties in UpdateProductCommand

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Products/UpdateProduct/UpdateProductCommand.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Products/UpdateProduct/UpdateProductCommand.cs
@@ -1,13 +1,16 @@
-﻿using MediatR;
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using MediatR;
 
 namespace Ambev.DeveloperEvaluation.Application.Products.UpdateProduct
 {
     public class UpdateProductCommand : IRequest<UpdateProductResponse>
     {
-        public int Id { get; }
-        public UpdateProductCommand(int id)
-        {
-            Id = id;
-        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public Rating Rating { get; set; }
     }
 }
